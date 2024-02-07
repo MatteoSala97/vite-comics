fd9daeb0594f9e3e6716685a025336d960bed885
MatteoSala97 <matteo111997@gmail.com>
2024-02-07 14:01:57 +0100

    config

---
+++ vite-comics/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import ExampleName from "./components/ExampleName.vue";
+
+export default {
+  components: {
+    ExampleName,
+  },
+};
+</script>
+
+<template>
+  <h1>Hello World</h1>
+  <ExampleName />
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
